b699ab81082c797bfc7958b51b3f7bdaff902bf9
HunterHenry67 <enrique.madrid262804@potros.itson.edu.mx>
2026-06-23 23:49:25 -0700

    Muchos Cambios
    Creación de Entidades, Conexión a base de datos.

---
+++ LaboratorioSaludTotal_Entidades/src/main/java/Entidades/Cliente.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entidades;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author BALAMRUSH
+ */
+@Entity
+@Table(name = "cliente")
+public class Cliente implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idCliente", nullable = false)
+    private Integer idCliente;
+    
+    @Column(name = "nombres", nullable = false, length = 50)
+    private String nombres;
+    
+    @Column(name = "apellidoPaterno", nullable = false, length = 50)
+    private String apellidoPaterno;
+    
+    @Column(name = "apellidoMaterno", nullable = false, length = 50)
+    private String apellidoMaterno;
+    
+    @Column(name = "sexo", nullable = false, length = 9)
+    private String sexo;
+    
+    @Column(name = "fechaNacimiento", nullable = false)
+    private LocalDateTime fechaNacimiento;
+    
+    @Column(name = "tipoSangre", nullable = false, length = 10)
+    private String tipoSangre;
+    
+    @OneToMany(mappedBy = "cliente", cascade = CascadeType.PERSIST)
+    private List<Prueba> listaPruebas = new ArrayList<>();
+    
+   
+    
+}
