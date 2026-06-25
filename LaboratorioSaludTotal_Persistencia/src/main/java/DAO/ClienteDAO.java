3ead67abb065d7c106b6bf881bc52771fc3e8754
increible <vortegalopez5@gmail.com>
2026-06-25 12:46:55 -0700

    modificaciones
    ClienteDAO implementaciones de metodos (nt)

--- LaboratorioSaludTotal_Persistencia/src/main/java/DAO/ClienteDAO.java
+++ LaboratorioSaludTotal_Persistencia/src/main/java/DAO/ClienteDAO.java
@@ -5,7 +5,11 @@
 package DAO;
 
 import Entidades.Cliente;
+import Entidades.Sexo;
+import java.time.LocalDateTime;
+import java.util.List;
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 
 /**
  *
@@ -19,19 +23,54 @@ public class ClienteDAO implements IClienteDAO{
     }
 
     @Override
-    public Cliente buscarClienteId(int id) throws Exception{
+    public Cliente buscarClienteId(int id) throws PersistenciaException{
         EntityManager em = conexion.conexionBD();
         
         try {
             Cliente cliente = em.find(Cliente.class, id);
             return cliente;
         } catch (Exception e) {
-            throw new Exception ("Error al buscar cliente por ID en DAO " + e.getMessage());
+            throw new PersistenciaException ("Error al buscar cliente por ID en DAO ");
         }finally {
             em.close();
         }
         
         
+    }
+
+    @Override
+    public List<Cliente> buscarClienteNombre(String nombre) throws PersistenciaException {
+        EntityManager em = conexion.conexionBD();
+    
+        try {
+            String jpql = "SELECT c FROM Cliente c WHERE c.nombre LIKE :paramNombre";
+
+            TypedQuery<Cliente> query = em.createQuery(jpql, Cliente.class);
+
+            query.setParameter("paramNombre", "%" + nombre + "%");
+
+            return query.getResultList();
+
+        } catch (Exception e) {
+            throw new PersistenciaException("Error al buscar cliente por nombre en DAO");
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+    }
+
+    @Override
+    public List<Cliente> buscarClienteFechaNacimiento(LocalDateTime fecha) throws PersistenciaException {
+    }
+
+    @Override
+    public List<Cliente> buscarClienteTipoSangre(String tipoSangre) throws PersistenciaException {
+    }
+
+    @Override
+    public List<Cliente> buscarClienteSexo(Sexo sexo) throws PersistenciaException {
+    }
+    
     }
     
 }
