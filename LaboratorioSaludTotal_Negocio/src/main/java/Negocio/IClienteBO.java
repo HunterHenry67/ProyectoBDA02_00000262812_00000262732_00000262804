65bdb790404baafb2b455f7d748cc4b7078833a2
increible <vortegalopez5@gmail.com>
2026-06-25 01:42:32 -0700

    modificaciones
    modificaciones de clienteDAO y Cliente Negocio

--- LaboratorioSaludTotal_Negocio/src/main/java/Negocio/IClienteBO.java
+++ LaboratorioSaludTotal_Negocio/src/main/java/Negocio/IClienteBO.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package DAO;
+package Negocio;
 
 import Entidades.Cliente;
 
@@ -10,7 +10,6 @@ import Entidades.Cliente;
  *
  * @author user
  */
-public interface IClienteDAO {
-    
+public interface IClienteBO {
     Cliente buscarClienteId (int id) throws Exception;
 }
--- LaboratorioSaludTotal_Persistencia/src/main/java/DAO/ClienteDAO.java
+++ LaboratorioSaludTotal_Persistencia/src/main/java/DAO/ClienteDAO.java
@@ -5,29 +5,33 @@
 package DAO;
 
 import Entidades.Cliente;
+import javax.persistence.EntityManager;
 
 /**
  *
  * @author BALAMRUSH
  */
 public class ClienteDAO implements IClienteDAO{
+    private IConexionBD conexion;
+    
+    public ClienteDAO (){
+        this.conexion = conexion;
+    }
 
     @Override
-    public Cliente buscarClienteId(int id) {
+    public Cliente buscarClienteId(int id) throws Exception{
+        EntityManager em = conexion.conexionBD();
         
-        String sql = """
-                     Select
-                        idCliente,
-                        nombre,
-                        apellidoPaterno,
-                        apellidoMaterno,
-                        fechaNacimiento,
-                        tipoSangre,
-                        sexo
-                     FROM Cliente
-                     Where idCliente = ?
-                     """;
-        return null;
+        try {
+            Cliente cliente = em.find(Cliente.class, id);
+            return cliente;
+        } catch (Exception e) {
+            throw new Exception ("Error al buscar cliente por ID en DAO " + e.getMessage());
+        }finally {
+            em.close();
+        }
+        
+        
     }
     
 }
