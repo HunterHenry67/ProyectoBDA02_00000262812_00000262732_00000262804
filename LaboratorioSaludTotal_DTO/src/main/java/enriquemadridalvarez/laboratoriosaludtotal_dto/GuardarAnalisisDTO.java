1b6b6bf4d67a81346d59f088500a13676c907e25
Andreiy Montoya Navarro <andreiymn02@gmail.com>
2026-06-25 00:41:15 -0700

    GuardarAnalisis

---
+++ LaboratorioSaludTotal_DTO/src/main/java/enriquemadridalvarez/laboratoriosaludtotal_dto/GuardarAnalisisDTO.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package enriquemadridalvarez.laboratoriosaludtotal_dto;
+
+/**
+ *
+ * @author Andre
+ */
+public class GuardarAnalisisDTO {
+    
+}
