70ca74d2feb6d08ecb9fa02bf53f697eebf81ab2
HunterHenry67 <enrique.madrid262804@potros.itson.edu.mx>
2026-06-25 18:49:18 -0700

    Resultado DAO

--- LaboratorioSaludTotal_Persistencia/src/main/java/DAO/ResultadoDAO.java
+++ LaboratorioSaludTotal_Persistencia/src/main/java/DAO/ResultadoDAO.java
@@ -4,10 +4,57 @@
  */
 package DAO;
 
+import Entidades.Resultado;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceException;
+
 /**
  *
  * @author BALAMRUSH
  */
-public class ResultadoDAO {
+public class ResultadoDAO implements IResultadoDAO{
+
+    private IConexionBD conexionBD;
+    
+    public ResultadoDAO(IConexionBD conexionBD){
+        this.conexionBD = conexionBD;
+    }
+    
+    @Override
+    public Resultado registrarResultado(Resultado resultado) throws PersistenciaException {
+        EntityManager entityManager = conexionBD.conexionBD();
+        try{
+            entityManager.getTransaction().begin();
+            entityManager.persist(resultado);
+            entityManager.getTransaction().commit();
+            return resultado;
+        }catch(Exception ex){
+            entityManager.getTransaction().rollback();
+            throw new PersistenciaException("Error al registrar el resultado: "+ex.getMessage());
+        }finally{
+            entityManager.close();
+        }
+    }
+
+    @Override
+    public Resultado consultarResultadoPorID(Integer idResultado) throws PersistenciaException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<Resultado> consultarResultadoPorPrueba() throws PersistenciaException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List consultarResultadoPorPruebaParametro(Integer idPrueba, Integer idParametro) throws PersistenciaException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean resultadoExiste(Integer idPrueba, Integer idParametro) throws PersistenceException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
